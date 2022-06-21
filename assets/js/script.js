785025915bfe032d679beaae96cdb5364b9b3b62
Craig <craig.skicko@gmail.com>
2022-06-22 09:01:04 +1000

    Added code plan

---
+++ assets/js/script.js
@@ -0,0 +1,22 @@
+// Plan
+
+// Flow
+// 1. Get the current saved items from local storage
+// 2. Render each time slot on the page as an inline form
+// 3. Color each time slot depending on time of day
+// 4. Add each local storage item to the appropriate time slot
+// 5. Allow the user to update the text in any of the time slots
+// 6. Save the text inputted into a time slot when the save button is clicked
+// 7. Each minute, read the time and update the time slot colours
+
+// Global Variables
+// Array of time slots to display
+// Activities variable to store activities (for retrieving from local storage then adding to the page)
+
+// Functions
+// 1. getSavedActivities (1)
+// 2. renderDayPlanner (2)
+// 3. colorTimeSlots (3)
+// 4. inputSavedItems (4)
+// 5. saveActivities (6)
+// 6. updateCurrentTime (7)
